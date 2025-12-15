130b1ef69df8e5235189e9300bcaced6d205998a
Lucas Duarte da Silva <102393984+LukazDs@users.noreply.github.com>
2025-12-15 08:57:53 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def ordenarValoresCrescentes(a, b, c):
+    
+    listaValores = [a, b, c]
+    
+    listaOrdenadaDesc = sorted(listaValores, reverse=True)
+
+    print(listaOrdenadaDesc)
+    
+ordenarValoresCrescentes(1, 7, 2)
